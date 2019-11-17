5390cd00b76515c66be29ca56ace4cac5159b51a
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-17 16:32:03 -0500

    create Client model

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'clients',
 
     # Required for auth
     'django.contrib.sites',
--- api/clients/models.py
+++ api/clients/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Client(models.Model):
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+  )
+  name = models.CharField(max_length=255)
