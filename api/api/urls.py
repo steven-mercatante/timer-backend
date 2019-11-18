6dfc9470d395cee5430e8a6bbf1c88c1a33fa79a
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-17 21:10:13 -0500

    start work on custom auth success view

--- api/api/urls.py
+++ api/api/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('clients.urls')),
+    path('', include('users.urls')),
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class AuthSuccess(TemplateView):
+  template_name = 'auth_success.html'
