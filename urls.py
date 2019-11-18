0f1bc541572fd210c05e0dd07ee1c0fceb986fee
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-18 11:03:17 -0500

    tweak token urls

--- urls.py
+++ urls.py
@@ -10,9 +10,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('clients.urls')),
     path('', include('users.urls')),
 ]
