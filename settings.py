0e7fd594a4d0e96d9ddda19c116e704a24556b11
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-18 12:23:35 -0500

    delete old JWT code

--- settings.py
+++ settings.py
@@ -116,7 +116,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
 
--- users/views.py
+++ users/views.py
@@ -1,15 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 from rest_framework.authtoken.models import Token
-# from rest_framework_simplejwt.tokens import RefreshToken
 
 
 def auth_success(request):
-    # refresh_token = RefreshToken.for_user(request.user)
-    # token = {
-    #     "refresh": str(refresh_token),
-    #     "access": str(refresh_token.access_token),
-    # }
     token = Token.objects.get(user_id=request.user.id)
     template = loader.get_template('auth_success.html')
 
