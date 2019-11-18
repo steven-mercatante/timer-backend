f31f125d853cae647e1e722bb79d5817f008ae39
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-18 15:19:44 -0500

    start working on logout view

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('users/auth-success', views.auth_success, name='auth-success'),
+    path('users/logout', views.LogOutView.as_view(), name='log_out'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,26 @@
 from django.http import HttpResponse
 from django.template import loader
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 def auth_success(request):
-    token = Token.objects.get(user_id=request.user.id)
+    try:
+        token = Token.objects.get(user_id=request.user.id)
+    except Token.DoesNotExist:
+        token = Token.objects.create(user=request.user)
     template = loader.get_template('auth_success.html')
 
     resp = HttpResponse(template.render({}, request))
     resp.set_cookie('auth_token', str(token))
 
     return resp
+
+
+class LogOutView(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        resp = Response('logged out')
+        resp.delete_cookie('auth_token')
+        return resp
